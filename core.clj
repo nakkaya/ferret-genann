841eb04dfa1d32eae7226a361444a9c92dde6886
Nurullah Akkaya <nurullah@nakkaya.com>
2017-05-01 12:51:14 +0300

    implement read/write network to file

--- core.clj
+++ core.clj
@@ -8,11 +8,26 @@
         "number_t _num_outputs;")
   
   (equals "return obj<boolean>(_network == o.cast<genann_o>()->network());")
-  (stream_console "runtime::print(\"genann\"); return nil();")
+  (stream_console "runtime::print(\"genann<\");
+                   runtime::print(num_inputs);
+                   runtime::print(\" \");
+                   runtime::print(num_hidden_layers);
+                   runtime::print(\" \");
+                   runtime::print(num_hidden);
+                   runtime::print(\" \");
+                   runtime::print(_num_outputs);
+                   runtime::print(\">\");
+                   return nil();")
   (fns
    ("explicit genann_o(number_t i, number_t hl, number_t h, number_t o) 
                   : num_inputs(i), num_hidden_layers(hl), num_hidden(h), _num_outputs(o){ 
        _network = genann_init(num_inputs, num_hidden_layers, num_hidden, _num_outputs);
+     }")
+   ("explicit genann_o(genann *ann) : _network(ann){ 
+      num_inputs = _network->inputs;
+      num_hidden_layers = _network->hidden_layers;
+      num_hidden = _network->hidden;
+      _num_outputs = _network->outputs;
      }")
    ("genann *network() const { return _network; }")
    ("number_t num_outputs() const { return _num_outputs; }")
@@ -59,3 +74,21 @@
    number_t size = nn.cast<genann_o>()->num_outputs();
    for(number_t i = --size; i >= 0; i--)
      __result = runtime::cons(obj<number>(ret[i]),__result);")
+
+(defn write [nn f]
+  "genann *ann = nn.cast<genann_o>()->network();
+   FILE *file = fopen(string::to<std::string>(f).c_str(), \"w\");
+
+   if (file == NULL)
+     return nil();
+ 
+   genann_write(ann,file);
+   __result = obj<boolean>(true);")
+
+(defn read [f]
+  "FILE *file = fopen(string::to<std::string>(f).c_str(), \"r\");
+
+   if (file == NULL)
+     return nil();
+
+   __result  = obj<genann_o>(genann_read(file));")
