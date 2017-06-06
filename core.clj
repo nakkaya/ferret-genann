3f290146d1597f473875cd33ab4eae0262907245
Nurullah Akkaya <nurullah@nakkaya.com>
2017-06-06 18:33:42 +0300

    use non depricated defobject api

--- core.clj
+++ core.clj
@@ -1,38 +1,6 @@
 (native-header "ferret-genann/genann/genann.h")
 
-(defobject genann_o
-  (data "genann *_network;"
-        "number_t num_inputs;"
-        "number_t num_hidden_layers;"
-        "number_t num_hidden;"
-        "number_t _num_outputs;")
-  
-  (equals "return obj<boolean>(_network == o.cast<genann_o>()->network());")
-  (stream_console "runtime::print(\"genann<\");
-                   runtime::print(num_inputs);
-                   runtime::print(\" \");
-                   runtime::print(num_hidden_layers);
-                   runtime::print(\" \");
-                   runtime::print(num_hidden);
-                   runtime::print(\" \");
-                   runtime::print(_num_outputs);
-                   runtime::print(\">\");
-                   return nil();")
-  (fns
-   ("explicit genann_o(number_t i, number_t hl, number_t h, number_t o) 
-                  : num_inputs(i), num_hidden_layers(hl), num_hidden(h), _num_outputs(o){ 
-       _network = genann_init(num_inputs, num_hidden_layers, num_hidden, _num_outputs);
-     }")
-   ("explicit genann_o(genann *ann) : _network(ann){ 
-      num_inputs = _network->inputs;
-      num_hidden_layers = _network->hidden_layers;
-      num_hidden = _network->hidden;
-      _num_outputs = _network->outputs;
-     }")
-   ("genann *network() const { return _network; }")
-   ("number_t num_outputs() const { return _num_outputs; }")
-   ("~genann_o(){ genann_free(_network); }"))
-  (force-type))
+(defobject genann_o "ferret-genann/genann_o.h")
 
 (defn init [input hidden-layers hidden outputs]
   "__result  = obj<genann_o>(number::to<number_t>(input), 
---
+++ genann_o.h
@@ -0,0 +1,44 @@
+class genann_o : public object_t {
+  genann *_network;
+  number_t num_inputs;
+  number_t num_hidden_layers;
+  number_t num_hidden;
+  number_t _num_outputs;
+
+public:
+  size_t type() const { return runtime::type::genann_o; }
+
+  bool equals(var o) const {
+    return (_network == o.cast<genann_o>()->network());
+  }
+
+#if !defined(FERRET_DISABLE_STD_OUT)
+  var stream_console() const {
+    runtime::print("genann<");
+    runtime::print(num_inputs);
+    runtime::print(" ");
+    runtime::print(num_hidden_layers);
+    runtime::print(" ");
+    runtime::print(num_hidden);
+    runtime::print(" ");
+    runtime::print(_num_outputs);
+    runtime::print(">");
+    return nil();
+  }
+#endif
+
+  explicit genann_o(number_t i, number_t hl, number_t h, number_t o)
+    : num_inputs(i), num_hidden_layers(hl), num_hidden(h), _num_outputs(o) {
+    _network =
+      genann_init(num_inputs, num_hidden_layers, num_hidden, _num_outputs);
+  }
+  explicit genann_o(genann *ann) : _network(ann) {
+    num_inputs = _network->inputs;
+    num_hidden_layers = _network->hidden_layers;
+    num_hidden = _network->hidden;
+    _num_outputs = _network->outputs;
+  }
+  genann *network() const { return _network; }
+  number_t num_outputs() const { return _num_outputs; }
+  ~genann_o() { genann_free(_network); }
+};
